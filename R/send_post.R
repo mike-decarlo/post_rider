d54061304bed3d50b8d0c1bd72fc9f3abccd6cb5
Mike <8523449+wolfm4ne@users.noreply.github.com>
2020-03-19 14:51:58 -0400

    Intial contributions

---
+++ R/send_post.R
@@ -0,0 +1,50 @@
+#' @title Send Post
+#'
+#' @description Send a message to a Slack channel using R and webhooks.
+#' @param .letter a string, default: \code{"Hello world!"}; should be enclosed
+#'   in quotes and contain the user's message
+#' @param .rider a string, default: \code{NULL}; should be enclosed in quotes.
+#'   If left as NULL will fill in as \code{Sys.info()[["user"]]}
+#' @param .channel a string, default: \code{NULL}; should be enclosed in quotes
+#'   and contain the name of a valid environment variable containing a channel
+#'   webhook string the user/.rider has access to
+#' @param .webhook a string, default: \code{NULL}; recommend storing webhook
+#'   as a user environment variable or some other secure manner and extracting
+#'   as a character string to this argument
+#' @keywords slack message send post
+#' @examples
+#' send_post(.letter = "foo", .channel = "bar")
+#' @importFrom stringr str_c
+#' @importFrom jsonlite validate
+#' @importFrom httr POST
+#' @importFrom RCurl curlUnescape
+#' @export
+send_post <- function(.letter = "Hello world!", .rider = NULL
+                 , .channel = NULL, .webhook = NULL) {
+  .post <- stringr::str_c(
+    '{"text":"'
+    , if (is.null(.rider)) {
+      Sys.info()[["user"]]
+    } else {
+      .rider
+    }
+    , ": "
+    , .letter
+    , '"}'
+  )
+  if (!jsonlite::validate(post)) {
+    stop("Not valid JSON text.")
+  } else {
+    .post <- httr::POST(
+      url = if (!is.null(.webhook)) {
+        .webhook
+      } else if (!is.null(.channel)) {
+        Sys.getenv(.channel)
+      } else {
+        stop("A channel environment variable or webhook must be supplied.")
+      }
+      , body = RCurl::curlUnescape(.post)
+    )
+    .post
+  }
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(post_rider)
+
+test_check("post_rider")
